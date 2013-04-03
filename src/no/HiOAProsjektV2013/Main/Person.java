d3dca4f8550f50471a1f467fa7eb28407a894ace
Joakim <Joakimrishaug@gmail.com>
2013-04-03 11:59:12 +0200

    Testupdate Epost

--- src/no/HiOAProsjektV2013/Main/Person.java
+++ src/no/HiOAProsjektV2013/Main/Person.java
@@ -1,11 +1,12 @@
 package no.HiOAProsjektV2013.Main;
 
 public class Person {
-	private String navn;
+	private String navn, epost;
 	private int telefonNr;
 	
-	public Person(String navn, int tlf){
+	public Person(String navn, String epost, int tlf){
 		this.navn = navn;
+		this.epost = epost;
 		telefonNr = tlf;
 	}
 
@@ -16,5 +17,9 @@ public class Person {
 	public int getTelefonNr() {
 		return telefonNr;
 	}
-
+	
+	public String getEpost(){
+		return epost;
+	}
+	
 }
